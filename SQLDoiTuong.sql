4c5a27558827237dcf4f6e694ca309c9aa7d8f8d
thaoly0310 <62956402+thaoly0310@users.noreply.github.com>
2020-04-11 23:13:24 +0700

    Delete SQLDoiTuong.sql

--- SQLDoiTuong.sql
+++
@@ -1,9 +0,0 @@
-create table tblDoiTuong
-(
-MaDoiTuong nvarchar(10) primary key,
-TenDoiTuong nvarchar(50),
-)
-insert into tblDoiTuong(MaDoiTuong, TenDoiTuong)
-values ('01', 'Nam')
-insert into tblDoiTuong(MaDoiTuong, TenDoiTuong)
-values ('01', 'Nu')
