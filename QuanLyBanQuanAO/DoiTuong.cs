f6630db747de6be501799af5520753db1bf676eb
thaoly0310 <thaoly.pandauniform@gmail.com>
2020-04-11 17:47:18 +0700

    add new file *

---
+++ QuanLyBanQuanAO/DoiTuong.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyBanQuanAO
+{
+    public partial class DoiTuong : Form
+    {
+        public DoiTuong()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SQLMua.sql
@@ -0,0 +1,13 @@
+create table tblMua
+(
+MaMua nvarchar(10) primary key,
+TenMua nvarchar(50),
+)
+insert into tblMua(MaMua, TenMua)
+values ('01', 'Xuan')
+insert into tblMua(MaMua, TenMua)
+values ('01', 'Ha')
+insert into tblMua(MaMua, TenMua)
+values ('01', 'Thu')
+insert into tblMua(MaMua, TenMua)
+values ('01', 'Dong')
